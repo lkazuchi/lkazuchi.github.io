b7f54df47a15f34b2420f1d4fd57fa6ff92a229f
lkazuchi <72500614+lkazuchi@users.noreply.github.com>
2021-02-16 17:46:36 +0100

    Add files via upload

--- worker.js
+++ worker.js
@@ -1,9 +1,6 @@
 onmessage = function (e) {
   console.log("Worker: Message received from main script");
   const result = e.data[0] * e.data[1];
-  let x = Math.random() * (max - min) + min;
-  let y = Math.random() * (max - min) + min;
-  drawSpot(x, y);
   if (isNaN(result)) {
     postMessage("Please write two numbers");
   } else {
@@ -12,15 +9,3 @@ onmessage = function (e) {
     postMessage(workerResult);
   }
 };
-
-const min = 400;
-const max = 400;
-
-function drawSpot(x, y) {
-  const c = document.getElementById("myCanvas");
-  let ctx = c.getContext("2d");
-  ctx.beginPath();
-  ctx.arc(10 * x, 10 * y, 5, 0, 2 * Math.PI);
-  console.log(x, y);
-  ctx.stroke();
-}
