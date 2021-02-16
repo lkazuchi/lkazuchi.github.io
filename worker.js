351a2d477bc2533c378a2c5760273967fe00a783
lkazuchi <72500614+lkazuchi@users.noreply.github.com>
2021-02-16 18:12:34 +0100

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,6 +1,17 @@
+function drawCircle(x, y) {
+  const c = document.getElementById("myCanvas");
+  let ctx = c.getContext("2d");
+  ctx.beginPath();
+  ctx.arc(10 * x, 10 * y, 5, 0, 2 * Math.PI);
+  ctx.stroke();
+}
+
 onmessage = function (e) {
   console.log("Worker: Message received from main script");
   const result = e.data[0] * e.data[1];
+  let x = e.data[0];
+  let y = e.data[1];
+  drawCircle(x, y);
   if (isNaN(result)) {
     postMessage("Please write two numbers");
   } else {
