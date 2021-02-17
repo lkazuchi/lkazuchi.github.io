39f94af6ccbca9f40edf19d83c6f0db9c363000c
lkazuchi <72500614+lkazuchi@users.noreply.github.com>
2021-02-17 12:08:28 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -25,3 +25,26 @@ if (window.Worker) {
 }
 
 //<link rel="stylesheet" href="style2.css" />
+let j = 0;
+let min = 0;
+let max = 0;
+const duree = 50;
+let interval = setInterval(function () {
+  let x = Math.random() * (max - min) + min;
+  let y = Math.random() * (max - min) + min;
+  first.postMessage([first.value, second.value]);
+  second.postMessage([first.value, second.value]);
+  console.log("Messages posted to worker");
+  if (++j === duree) {
+    clearInterval(interval);
+  }
+}, 1000);
+
+function drawCircle(x, y) {
+  const c = document.getElementById("myCanvas");
+  let ctx = c.getContext("2d");
+  ctx.beginPath();
+  ctx.arc(10 * x, 10 * y, 5, 0, 2 * Math.PI);
+  console.log(x, y);
+  ctx.stroke();
+}
